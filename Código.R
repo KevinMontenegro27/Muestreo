ed545cf840f7866d5037049f45363074b0b610d1
KevinMontenegro27 <143365974+KevinMontenegro27@users.noreply.github.com>
2025-11-16 13:09:28 -0600

    Add files via upload

---
+++ Código.R
@@ -0,0 +1,19 @@
+# Datos ----      
+load("act24_al_13nov.RData")
+
+
+##Nueva variable---- 
+act$factor= rep(1, 937)
+
+#Librerías ----
+library(dplyr)
+library(survey)
+library(labelled)
+
+
+
+#Tablas ----
+
+
+
+#Gráficos ----
